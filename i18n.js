2bbb9bb6bb89946dc46f530990f09205013dbfa8
Ced <conquete.asie@gmail.com>
2020-08-13 11:24:09 +0200

    add browser detection

--- i18n.js
+++ i18n.js
@@ -5,5 +5,13 @@ const path = require('path');
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
   localeSubpaths,
+  browserLanguageDetection: true,
+  serverLanguageDetection: true,
+  detection: {
+    lookupCookie: 'next-i18next',
+    order: ['cookie', 'localStorage', 'navigator', 'path', 'subdomain'],
+    caches: ['cookie'],
+    excludeCacheFor: ['cimode'],
+  },
   localePath: path.resolve('./public/static/locales'),
 });
