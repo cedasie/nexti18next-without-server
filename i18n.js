06f9871540d7b9ec0fc72127a9439ff30c3a5346
Ced <conquete.asie@gmail.com>
2020-08-18 14:52:53 +0200

    remove detector

--- i18n.js
+++ i18n.js
@@ -3,15 +3,15 @@ const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 const path = require('path');
 
 module.exports = new NextI18Next({
+  otherLanguages: ['en', 'de', 'fr'],
   localeSubpaths,
   localePath: path.resolve('./public/static/locales'),
   // strictMode: true,
   // defaultLanguage: 'en',
   fallbackLng: ['en', 'de', 'fr'],
-  otherLanguages: ['de', 'fr'],
   debug: true,
-  browserLanguageDetection: true,
-  serverLanguageDetection: true,
+  // browserLanguageDetection: true,
+  // serverLanguageDetection: true,
   customDetectors: ['cookie', 'localStorage'],
   detection: {
     lookupCookie: 'language',
@@ -24,7 +24,6 @@ module.exports = new NextI18Next({
       'querystring',
       'sessionStorage',
       'navigator',
-      'subdomain',
     ],
     // order: ['navigator', 'localStorage', 'cookie', 'path', 'subdomain'],
     caches: ['localStorage', 'cookie'],
